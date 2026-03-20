11100538f134647aeb65fdf68411808b6a9a1605
Nathaniel B. Balingkit <balingkit.nathaniel28@gmail.com>
2026-03-20 23:12:04 +0800

    updated rest framework

--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
 }
 
